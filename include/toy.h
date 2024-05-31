2e8a6ef585e4228458dec1423517b2668b47e3ac
Bruno Bertolino <bpbertolino@yahoo.com.br>
2024-05-31 10:26:53 -0300

    Mudanças nos semáforos de toys

--- include/toy.h
+++ include/toy.h
@@ -6,13 +6,8 @@
 // Você pode declarar novas funções aqui
 
 extern int num_toys;
-extern toy_t **thread_to_toy;
-extern sem_t *sem_toys;
 extern sem_t *sem_toys_enter;
-extern sem_t *sem_toys_leave;
-extern pthread_mutex_t map_lock;
-
-void initialize_thread_to_toy(toy_args *args, int i);
+extern pthread_mutex_t toy_lock;
 
 /********************************
  *          ATENÇÃO             *
