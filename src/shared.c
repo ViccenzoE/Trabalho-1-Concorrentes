8575737fec0088eef9a6a9a169108ad90f4c789c
Viccenzo <viccenzoescarrone@gmail.com>
2024-05-31 09:40:38 -0300

    Adicionada lógica (ainda não finalizada) no client.c

--- src/shared.c
+++ src/shared.c
@@ -6,6 +6,9 @@
 // Você pode declarar novas funções (ou variaveis compartilhadas) aqui
 
 pthread_mutex_t *mutex_cliente_fila; // um mutex para cada cliente??
+pthread_mutex_t *mutex_cliente_atendido;
+pthread_mutex_t bilheteiro_sem;
+
 
 /**********************************
  *          ATENÇÃO               *
